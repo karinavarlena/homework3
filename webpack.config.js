003d1c967d5b7c31f64423da494f0e03ef068867
karinavarlena <karina.varlena@gmail.com>
2020-06-15 22:59:45 +0300

    task3: Generating HTML using plugin, clearing dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'star wars',
+            template: 'index.html'
+
+        }),
+    ],
+    module: {
          rules: [
            {
                 test: /\.css$/,
@@ -22,5 +34,5 @@ module.exports = {
                 ],
             },
          ],
-       },
+    },
 };
